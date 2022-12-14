a3bb26df6a651c35b965577fb261b1418e0befd9
Akash Mishra <akashmishra242@gmail.com>
2022-12-15 01:05:21 +0530

    Project Initialized,authentication done.

---
+++ lib/feature/chats/screens/mobile_chat_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/utility/colors.dart';
+
+class MobileLayoutScreen extends StatelessWidget {
+  const MobileLayoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: backgroundColor,
+    );
+  }
+}
