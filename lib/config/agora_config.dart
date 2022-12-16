ceae9aeb6d8ff0faecea18f35c2d16a9c2cb8d32
Akash Mishra <akashmishra242@gmail.com>
2022-12-16 23:19:08 +0530

    Changed App Name & Icon.

---
+++ lib/config/agora_config.dart
@@ -0,0 +1,7 @@
+import 'package:whatsapp_clone/constants.dart';
+
+class AgoraConfig {
+  static String token = '';
+  static String appId = agoraappid;
+  static String appcertificate = agoraappcertificate;
+}
